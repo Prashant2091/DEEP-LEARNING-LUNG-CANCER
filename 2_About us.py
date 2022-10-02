754d806c49514a78757fea01426dc312e750680e
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2022-10-02 22:17:23 +0530

    Add files via upload

---
+++ 2_About us.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+#title
+st.title('Application of Deep Learning Models for Lung Cancer Type Detection')
+st.header("Group Members")
+st.markdown("Aditya Desai (220340128001)")
+st.markdown("Mayur Patil (220340128025)")
+st.markdown("Nikhil Londhe (220340128032)")
+st.markdown("Piyush Nagpure (220340128034)")
+st.markdown("Prashant Kumar (220340128035)")
+st.header("Guided By: Ms.Swapna E.")
+
+        	
+
+        
+
+
+
